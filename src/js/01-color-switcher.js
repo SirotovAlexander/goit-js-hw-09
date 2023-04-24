02e35b0183c02ed7449e907548af16657c4099be
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-24 13:55:07 +0300

    add=some
    add=some

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const bodyQwery = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', onStart);
+
+function onStart(event) {
+  startBtn.disabled = true;
+  setInterval(() => {
+    bodyQwery.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
