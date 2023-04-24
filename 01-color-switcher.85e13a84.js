46d2971101e5c42b885a7ead6c8484d9c6ca20be
SirotovAlexander <116684295+SirotovAlexander@users.noreply.github.com>
2023-04-24 10:55:44 +0000

    Deploying to gh-pages from @ SirotovAlexander/goit-js-hw-09@02e35b0183c02ed7449e907548af16657c4099be 🚀

---
+++ 01-color-switcher.85e13a84.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),o=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");o.addEventListener("click",(function(e){o.disabled=!0,setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.85e13a84.js.map
