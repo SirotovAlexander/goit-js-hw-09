8143d2ed4e2bdfc880107a836e91bd933f982136
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-24 14:44:12 +0300

    start third
    start third

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,15 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const submitBtn = document.querySelector('button[type="submit"]');
+
+console.log(form);
+console.log(delay);
+console.log(step);
+console.log(amount);
+console.log(submitBtn);
