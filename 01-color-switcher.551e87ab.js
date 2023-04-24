8cade9081d1b847516dfeea66b1529ffe10fc5fd
SirotovAlexander <116684295+SirotovAlexander@users.noreply.github.com>
2023-04-24 11:32:17 +0000

    Deploying to gh-pages from @ SirotovAlexander/goit-js-hw-09@a1719220f5d4c3f7ad77d3816619d9fb9052e3e5 🚀

--- 01-color-switcher.551e87ab.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]");document.querySelector("button[data-stop]");e.addEventListener("click",(function(o){e.disabled=!0,setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)}));
-//# sourceMappingURL=01-color-switcher.551e87ab.js.map
---
+++ 01-color-switcher.6e1b291b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");t.addEventListener("click",(function(r){r.target===e?(e.disabled=!0,o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)):r.target===a&&(e.disabled=!1,clearInterval(o))}));var o=null}();
+//# sourceMappingURL=01-color-switcher.6e1b291b.js.map
