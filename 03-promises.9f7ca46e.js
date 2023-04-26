3baf21f1072182c75973a2ae151c9167cffad315
SirotovAlexander <116684295+SirotovAlexander@users.noreply.github.com>
2023-04-26 13:30:48 +0000

    Deploying to gh-pages from @ SirotovAlexander/goit-js-hw-09@ec334490717145db9d425e07121ca4bd956154fb 🚀

--- 03-promises.9f7ca46e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),o=document.querySelector('input[name="delay"]'),t=document.querySelector('input[name="step"]'),n=document.querySelector('input[name="amount"]'),c=document.querySelector('button[type="submit"]');console.log(e),console.log(o),console.log(t),console.log(n),console.log(c);
-//# sourceMappingURL=03-promises.9f7ca46e.js.map
--- 03-promises.d7300ace.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form"),o=document.querySelector('input[name="delay"]'),n=document.querySelector('input[name="step"]'),t=document.querySelector('input[name="amount"]'),u=document.querySelector('button[type="submit"]');console.log(e),console.log(o),console.log(n),console.log(t),console.log(u)}();
-//# sourceMappingURL=03-promises.d7300ace.js.map
